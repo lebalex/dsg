43a7b988417a46f86d6948aac603f78f0d6201cf
lebalex <lebalex@mail.ru>
2020-09-19 07:45:08 +0300

    19.09.2020 12:45

---
+++ sale.php
@@ -0,0 +1,62 @@
+<?php
+include_once 'header.php';
+?>
+<section class="welcome_area">
+    <div class="fullscreen-bg">
+        <div class="overlay">
+
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12">
+                        <div class="hero-content">
+                            <h3>DSG Комплект</h3>
+                            <h6>Оплата и доставка</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <video loop="" muted="" autoplay="" poster="bg/vag6.jpg" class="fullscreen-bg__video">
+            <source src="bg/vag6.mp4" type="video/mp4">
+            <source src="bg/vag6.webm" type="video/webm">
+        </video>
+    </div>
+</section>
+
+<!--section class="welcome_area_sm bg-img background-overlay" style="background-image: url(img/bg-img/bg-1a.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="hero-content">
+                        <h2>DSG Комплект</h2>
+                        <h6>О нас</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section-->
+
+<section class="shop_grid_area section-padding-0-80">
+    <div class="overlay3 container">
+        <div class="row">
+
+
+            <div class="col-12 col-sm-6 col-md-12">
+                <h6>Оплата и доставка</h6>
+
+                <p></p>
+            </div>
+
+
+
+
+
+        </div>
+
+    </div>
+
+    <?php
+    include_once 'footer.php';
+    ?>
