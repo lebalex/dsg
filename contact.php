83ae458aec45fb8ea8a12c5613bcbb4d92d7a56e
lebalex <lebalex@mail.ru>
2020-09-19 06:25:16 +0300

    19.09.2020 11:25

--- contact.php
+++ contact.php
@@ -41,44 +41,16 @@ include_once 'header.php';
 <section class="shop_grid_area section-padding-0-80">
     <div class="overlay3 container">
         <div class="row">
-            <!--div class="col-12 col-md-4 col-lg-3">
-                <div class="shop_sidebar_area">
 
-                    <div class="widget catagory mb-50">
-
-    
-                        <div class="catagories-menu">
-                            <ul id="menu-content2" class="menu-content collapse show">
-               
-                                <li data-toggle="collapse" data-target="#clothing">
-
-                                    <ul class="sub-menu collapse show" id="clothing">
-                                        <li><a href="#">О магазине</a></li>
-                                        <li><a href="#">Обратная связь</a></li>
-
-                                    </ul>
-                                </li>
-
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div-->
 
             <div class="col-12 col-sm-6 col-md-12">
-                            <h6>Мы рады приветствовать вас на сайте нашей компании.</h6>
+            <h6>Вы можете нас найти по этому адресу</h6>
 
-                            <p>Наша компания была основана в 2016 году, а наш интернет-магазин стал первым интернет - магазином в России, осуществляющим on-line продажу комплектов для ремонта DSG 7. Компания специализируется на оптовой и розничной продаже ремкомплектов DSG 7 как для мастерских, так и для ремонта DSG своими руками.</p>
+                            <p>Иркутск, ул. Монтажная, 5а</p>
 
-                            <p>На данный момент мы представляем собой крупную компанию, владеющую интернет–магазином и имеющую в своей сети единый call-центр, который регулирует всю деятельность магазина, отдел продаж, службу доставки, широкий штат квалифицированных сборщиков, собственный склад c постоянным наличием необходимого запаса комплектов.</p>
-
-                            <p>За это время у нас сложились партнерские отношения с техцентрами и розничными магазинами, позволяющие предлагать высококачественную продукцию и услуги по конкурентоспособным ценам в каждом регионе России.</p>
-
-                            <p>Мы можем гордиться тем, что мы первый интернет - магазин комплектов DSG 7 KINERGO ®</p>
-
-
-                            <p>Мы всегда рады общению с нашими клиентами. Если у вас есть какие-либо пожелания, предложения, замечания, касающиеся работы нашего Интернет-магазина - пишите нам, и мы с благодарностью примем ваше мнение во внимание.</p>
+                            <p>+7телефон</p>
+                            <p>+7телефон</p>
+                            <p>почта</p>
                         </div>
 
 
@@ -87,39 +59,20 @@ include_once 'header.php';
             
                 </div>
                 <div class="row">
-                <div class="col-12 col-sm-6 col-md-6">
-                    <h6>Мы всегда рады вашим вопросам и предложениям!</h6>
-                    <form action="#" method="post">
-                       
-                            <div class="col-12 mb-3">
-                                    <label for="first_name">Имя <span>*</span></label>
-                                    <input type="text" class="form-control" id="first_name" value="" required>
-                                </div>
-                                <div class="col-12 mb-3">
-                                    <label for="phone_number">Телефон</label>
-                                    <input type="number" class="form-control" id="phone_number" min="0" value="">
-                                </div>
-                                <div class="col-12 mb-3">
-                                    <label for="last_name">Email<span>*</span></label>
-                                    <input type="text" class="form-control" id="email_address" value="" required>
-                                </div>
-                                <div class="col-12 mb-3">
-                                    <label for="last_name">Коментарий<span>*</span></label>
-                                    <textarea class='form-control' id="comments" required></textarea>
-                                </div>
-                                <div class="col-12 mb-3">
-                                <div class="custom-control custom-checkbox d-block mb-2">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Я согласен на обработку персональных данных! <a href="#">Правила обработки</a></label>
-                                    </div>
-                                </div>
-
-                                <div class="col-12 mb-3">
-                                    <a href="#" class="btn essence-btn">Отправить</a>
-                                </div>
-
-                            
-                        </form>
+                <div class="col-12 col-sm-6 col-md-12">
+                    
+<!-- 2gis -->
+<div class="text-center">
+                    <a class="dg-widget-link" href="http://2gis.ru/irkutsk/firm/70000001020973347/center/104.19401407241821,52.25881997928739/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть на карте Иркутска</a>
+                    <div class="dg-widget-link">
+                        <a href="http://2gis.ru/irkutsk/firm/70000001020973347/photos/70000001020973347/center/104.19401407241821,52.25881997928739/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии компании</a>
+                    </div>
+                    <div class="dg-widget-link"><a href="http://2gis.ru/irkutsk/center/104.194017,52.258566/zoom/16/routeTab/rsType/bus/to/104.194017,52.258566╎VAGON, автосервис?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти проезд до VAGON, автосервис</a>
+                </div>
+                <script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+                <script charset="utf-8">new DGWidgetLoader({"width":'100%',"height":600,"borderColor":"#a3a3a3","pos":{"lat":52.25881997928739,"lon":104.19401407241821,"zoom":16},"opt":{"city":"irkutsk"},"org":[{"id":"70000001020973347"}]});</script>
+                <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
+                    </div>
                 </div>
                 </div>
             </div>
