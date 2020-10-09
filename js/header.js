d6b1edcf3c24f5a383f9f8dc9ef2e217c56fd9c4
lebalex <lebalex@mail.ru>
2020-10-09 14:41:14 +0300

    9.10.2020 19:41

--- js/header.js
+++ js/header.js
@@ -21,13 +21,13 @@
                     /*console.log(data.code);
                     console.log('ok')*/
                     $('#count_in_favouritet').text(data.favouritet);
-                    $('#login_exit_bth').html('<a href="/users/account"><img src="/img/core-img/user_login.svg" alt="" title="Личный кабинет - ${data.name}"></a>');
-                    $('#exit-area').html('<a href="/logout"  id="logoutBtn"><img src="/img/core-img/door-exit.svg" alt="">');
+                    $('#login_exit_bth').html(`<a href="/users/account"><img src="/img/core-img/user_login.svg" alt="" title="Личный кабинет - ${data.name}"></a>`);
+                    $('#exit-area').html(`<a href="/logout"  id="logoutBtn"><img src="/img/core-img/door-exit.svg" alt="">`);
                 }else
                 {
                     $('#count_in_favouritet').text('');
-                    $('#login_exit_bth').html('<a href="#1" style="color:#18479f;font-size:13px" data-toggle="modal" data-target="#myLogin">\
-                        <img src="/img/core-img/user.svg" alt="" title="Вход для зарегистрированных пользователей"></a>');
+                    $('#login_exit_bth').html(`<a href="#1" style="color:#18479f;font-size:13px" data-toggle="modal" data-target="#myLogin">
+                        <img src="/img/core-img/user.svg" alt="" title="Вход для зарегистрированных пользователей"></a>`);
                         $('#exit-area').html('');
 
 
@@ -54,14 +54,14 @@
             url: url,
             data: form.serialize(),
             success: function (data) {
-                console.log(data);
+                /*console.log(data);*/
                 if (data.code === 0) {
                     localStorage.setItem('jwt', data.jwt);
                     sessionStorage.setItem("sid",data.jwt)
                     $('.modal').modal('hide');
                     $('#count_in_favouritet').text(data.favouritet);
-                    $('#login_exit_bth').html('<a href="/users/account"><img src="/img/core-img/user_login.svg" alt="" title="Личный кабинет - ${data.name}"></a>');
-                    $('#exit-area').html('<a href="/logout"  id="logoutBtn"><img src="/img/core-img/door-exit.svg" alt="">');
+                    $('#login_exit_bth').html(`<a href="/users/account"><img src="/img/core-img/user_login.svg" alt="" title="Личный кабинет - ${data.name}"></a>`);
+                    $('#exit-area').html(`<a href="/logout"  id="logoutBtn"><img src="/img/core-img/door-exit.svg" alt="">`);
                 } else {
                     $('#login').addClass('is-invalid');
                     $('#p').addClass('is-invalid');
--- js/index.js
+++ js/index.js
@@ -35,19 +35,19 @@ function addFavouritet(e, product_id) {
 function createDivTopProduct(name, id, img, coast, id_categ, active) {
     if (img === null) img = "noPhoto.png";
     if (coast === null) coast = 0;
-    return '<div class="single-product-wrapper">\
-        <div class="product-img">\
-        <img src="/img/product-img/${img}" alt="">\
-        <div class="product-favourite"><a onclick="addFavouritet(this, ${id})" class="favme ${active} fa fa-heart"></a></div>\
-        </div>\
-        <div class="product-description">\
-        <a href="/catalog/${id_categ}/${id}">\
-        <h6>${name}</h6>\
-        </a>\
-        <p class="product-price">${new Intl.NumberFormat("ru-RU", {style: "currency",currency: "RUB",}).format(coast)}</p>\
-        <div class="hover-content">\
-        <div class="add-to-cart-btn"><button onclick=addChart(${id}) class="btn essence-btn">В корзину</button>\
-        </div>        </div>        </div>        </div>';
+    return `<div class="single-product-wrapper">
+        <div class="product-img">
+        <img src="/img/product-img/${img}" alt="">
+        <div class="product-favourite"><a onclick="addFavouritet(this, ${id})" class="favme ${active} fa fa-heart"></a></div>
+        </div>
+        <div class="product-description">
+        <a href="/catalog/${id_categ}/${id}">
+        <h6>${name}</h6>
+        </a>
+        <p class="product-price">${new Intl.NumberFormat('ru-RU', {style: 'currency',currency: 'RUB',}).format(coast)}</p>
+        <div class="hover-content">
+        <div class="add-to-cart-btn"><button onclick=addChart(${id}) class="btn essence-btn">В корзину</button>
+        </div>        </div>        </div>        </div>`;
 }
 
 $(document).ready(function() {
