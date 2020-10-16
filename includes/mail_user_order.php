507f848045ae69d9db868cc0de52cd9b2b33c2ee
lebalex <lebalex@mail.ru>
2020-10-16 06:56:14 +0300

    16.10.2020 11:56

--- includes/mail_user_order.php
+++ includes/mail_user_order.php
@@ -1,48 +1,115 @@
-
 <html>
-    <head>
-</style>
-    </head>
-    <body style="background-color: silver;">
-        <div style="width: 699px;background-color: snow;font-size: 12px;font-family: Verdana, Arial, Helvetica, sans-serif;margin: 10px auto 15px auto;">
-            <div style="padding: 30px;">
-            Ваш заказ в DSG Комплект
-            <div>
-                <h3>
-                    <a href="https://www.dsgkomplekt.ru"><img src="https://www.dsgkomplekt.ru/img/core-img/logo_dsg.png"/></a>
-                </h3>
-            </div>
-            <div><h4>Спасибо за Ваш заказ.</h4></div>
-<table>
 
-    <tr><td>Имя:</td><td><?=$name?></td></tr>
-    <tr><td>email:</td><td><?=$email?></td></tr>
-    <tr><td>Телефон:</td><td><?=$phone?></td></tr>
-    <tr><td>Комvентарий к заказу:</td><td><?=$description?></td></tr>
-</table>
-<div style="padding-top: 10px;"></div>
-<div><h4><?=$order_number?></h4></div>
-<table border="1"><tr><th style="text-align: left;">Наименование</th><th style="text-align: left;">OEM</th><th style="text-align: left;">Кол-во</th><th style="text-align: left;">Цена</th></tr>
-<?php $sum=0;
-foreach ($items as $item): ?>
-    <tr><td><?=$item->name?></td><td><?=$item->oem?></td><td><?=$item->count?></td><td><?=$item->coast?></td></tr>
-<?php 
-$sum += ($item->count * $item->coast);
-endforeach; ?>
-</table>
+<head>
+    </style>
+</head>
 
-<div><h4>Итого:<?=$sum?></h4></div>
-<div style="padding-top: 10px;">
-                            <h6>+7(3952)768-765</h6>
-                            <h6>+7(901)65 88 765</h6>
-                            <h6><a href="mailto:dsgkomplekt@mail.ru">dsgkomplekt@mail.ru</a></h6>
-                        </div>
+<body style="background-color: silver;">
 
-<div style="padding-top: 10px;">
-С уважением, сотрудники DSG Комплект
-</div>
-</div>
-        </div>
-        
-    </body>
+
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td>
+                <table style="background-color: snow;font-size: 12px;font-family: Verdana, Arial, Helvetica, sans-serif;margin: 10px auto 15px auto; padding: 10px;" border="0" width="700" cellspacing="0" cellpadding="0" align="center">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div style="padding-top: 10px;padding-bottom: 10px;">Ваш заказ в DSG Комплект</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h3>
+                                    <a href="https://www.dsgkomplekt.ru"><img src="https://www.dsgkomplekt.ru/img/core-img/logo_dsg.png" /></a>
+                                </h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>Спасибо за Ваш заказ.</h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table>
+
+                                    <tr>
+                                        <td>Имя:</td>
+                                        <td><?= $name ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>email:</td>
+                                        <td><?= $email ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Телефон:</td>
+                                        <td><?= $phone ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Комvентарий к заказу:</td>
+                                        <td><?= $description ?></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div style="padding-top: 10px;">
+                                    <div>
+                                        <h4><?= $order_number ?></h4>
+                                    </div>
+                                    <table border="1">
+                                        <tr>
+                                            <th style="text-align: left;">Наименование</th>
+                                            <th style="text-align: left;">OEM</th>
+                                            <th style="text-align: left;">Кол-во</th>
+                                            <th style="text-align: left;">Цена</th>
+                                        </tr>
+                                        <?php $sum = 0;
+                                        foreach ($items as $item) : ?>
+                                            <tr>
+                                                <td><?= $item->name ?></td>
+                                                <td><?= $item->oem ?></td>
+                                                <td><?= $item->count ?></td>
+                                                <td><?= $item->coast ?></td>
+                                            </tr>
+                                        <?php
+                                            $sum += ($item->count * $item->coast);
+                                        endforeach; ?>
+                                    </table>
+
+                                    <div>
+                                        <h4>Итого:<?= $sum ?></h4>
+                                    </div>
+
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style="padding-top: 10px;">
+                                    <h4>+7(3952)768-765</h4>
+                                    <h4>+7(901)65 88 765</h4>
+                                    <h4><a href="mailto:dsgkomplekt@mail.ru">dsgkomplekt@mail.ru</a></h4>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style="padding-top: 10px; padding-bottom: 40px;">
+                                    С уважением, сотрудники DSG Комплект
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+
+
+</body>
+
 </html>
--- includes/mail_user_reg.php
+++ includes/mail_user_reg.php
@@ -8,10 +8,10 @@
     <table border="0" cellpadding="0" cellspacing="0" width="100%">
         <tr>
             <td>
-                <table style="background-color: snow;font-size: 12px;font-family: Verdana, Arial, Helvetica, sans-serif;margin: 10px auto 15px auto;" border="0" width="600" cellspacing="0" cellpadding="0" align="center">
+                <table style="background-color: snow;font-size: 12px;font-family: Verdana, Arial, Helvetica, sans-serif;margin: 10px auto 15px auto; padding: 10px;" border="0" width="700" cellspacing="0" cellpadding="0" align="center">
                     <tbody>
                         <tr>
-                            <td>вы зарегистрировались на сайте DSG Комплект</td>
+                            <td><div style="padding-top: 10px;padding-bottom: 10px;">вы зарегистрировались на сайте DSG Комплект</div></td>
                         </tr>
                         <tr>
                             <td>
@@ -60,15 +60,15 @@
                         <tr>
                             <td>
                                 <div style="padding-top: 10px;">
-                                    <h6>+7(3952)768-765</h6>
-                                    <h6>+7(901)65 88 765</h6>
-                                    <h6><a href="mailto:dsgkomplekt@mail.ru">dsgkomplekt@mail.ru</a></h6>
+                                    <h4>+7(3952)768-765</h4>
+                                    <h4>+7(901)65 88 765</h4>
+                                    <h4><a href="mailto:dsgkomplekt@mail.ru">dsgkomplekt@mail.ru</a></h4>
                                 </div>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <div style="padding-top: 10px;">
+                                <div style="padding-top: 10px; padding-bottom: 40px;">
                                     С уважением, сотрудники DSG Комплект
                                 </div>
                             </td>
